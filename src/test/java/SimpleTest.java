24234fa4c4cf864ff3fede1786915f474e078d79
AndersonNY <56901619+AndersonNY@users.noreply.github.com>
2020-02-23 12:22:05 -0500

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+
+    public void SimpleTest() {
+
+        System.out.println("This test will always pass");
+
+    }
+
+}
